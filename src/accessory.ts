8f665efda2a62d887ee5a28d78d6c8a0720a8ad1
Michael Mouchous <michaelm11188@gmail.com>
2023-12-01 10:28:02 +0100

    disabling temporarly the homekit

--- src/accessory.ts
+++ src/accessory.ts
@@ -188,10 +188,10 @@ export class GpioDoorbellAccessory implements AccessoryPlugin {
           if (!this.config.enableHttpTrigger || !this.config.httpTriggerUrl) {
             // ring in homekit
             this.log.info('Forwarding ring directly to HomeKit.');
-            this.doorbellService.updateCharacteristic(
-              this.api.hap.Characteristic.ProgrammableSwitchEvent,
-              this.api.hap.Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS,
-            );
+            //this.doorbellService.updateCharacteristic(
+            //  this.api.hap.Characteristic.ProgrammableSwitchEvent,
+            //  this.api.hap.Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS,
+            //);
           } else {
             // ring via 3rd party plugin
             const url = this.config.httpTriggerUrl;
