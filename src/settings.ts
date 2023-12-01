e00bbd55983d11889b48287f45855cb893eef1a4
Michael Mouchous <michaelm11188@gmail.com>
2023-12-01 13:32:44 +0100

    update plugin name

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const ACCESSORY_NAME = 'GpioDoorbell';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-gpio-doorbell';
+export const PLUGIN_NAME = 'homebridge-gpio-doorbell-mick111';
